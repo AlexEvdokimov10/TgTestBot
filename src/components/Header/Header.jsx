39d705ab75832e4d5c351e19da3884ae3b186906
AlexEvdokimov10 <evdokimoffsanya5@gmail.com>
2024-06-11 00:44:48 +0300

    Add Browser Router

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,18 +3,14 @@ import Button from "../Button/Button";
 import {useTelegram} from "../../hooks/useTelegram";
 import './Header.css';
 
-
 const Header = () => {
-
-    const {tg, user, onClose} = useTelegram()
+    const {user, onClose} = useTelegram();
 
     return (
         <div className={'header'}>
-            <Button onClick={onClose}>
-                Close
-            </Button>
+            <Button onClick={onClose}>Закрыть</Button>
             <span className={'username'}>
-                {tg.initDataUnsafe?.user?.username}
+                {user?.username}
             </span>
         </div>
     );
