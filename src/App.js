728051af966f5b46f24d2738a12d390fad6ada06
AlexEvdokimov10 <evdokimoffsanya5@gmail.com>
2024-06-11 10:39:43 +0300

    Delete unused properties

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import './App.css';
 import {useEffect} from "react";
 import {useTelegram} from "./hooks/useTelegram";
 import Header from "./components/Header/Header";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import ProductList from "./components/ProductList/ProductList";
 import Form from "./components/Form/Form";
 
 
 function App() {
 
-    const {onToggleButton, tg} = useTelegram();
+    const {tg} = useTelegram();
 
 
     useEffect(() => {
